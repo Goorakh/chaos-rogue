ad200ff32024c6fb15f5b6686ebe53cf4257dbb9
Petter Öhlén <petter.ohlen@gmail.com>
2022-09-04 13:02:29 +0200

    Add basic player controller

---
+++ Assets/Scripts/MathUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathUtils
+{
+    public static float AngleTo(Vector2 a, Vector2 b)
+    {
+        return Mathf.Atan2(b.y - a.y, b.x - a.x) * Mathf.Rad2Deg;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera Camera;
+
+    public Transform AimCircle;
+
+    public float MoveSpeed;
+    public float SprintSpeedMultiplier;
+
+    public float SprintAcceleration;
+    public float SprintDecceleration;
+
+    float _currentSprintSpeedMultiplier = 1f;
+
+    void Update()
+    {
+        Vector3 movementInput = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            movementInput.z += 1f;
+
+        if (Input.GetKey(KeyCode.S))
+            movementInput.z -= 1f;
+
+        if (Input.GetKey(KeyCode.D))
+            movementInput.x += 1f;
+
+        if (Input.GetKey(KeyCode.A))
+            movementInput.x -= 1f;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (_currentSprintSpeedMultiplier != SprintSpeedMultiplier)
+            {
+                _currentSprintSpeedMultiplier = Mathf.MoveTowards(_currentSprintSpeedMultiplier, SprintSpeedMultiplier, SprintAcceleration * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (_currentSprintSpeedMultiplier != 1f)
+            {
+                _currentSprintSpeedMultiplier = Mathf.MoveTowards(_currentSprintSpeedMultiplier, 1f, SprintDecceleration * Time.deltaTime);
+            }
+        }
+
+        transform.position += movementInput.normalized * (MoveSpeed * _currentSprintSpeedMultiplier * Time.deltaTime);
+
+        Vector3 localEulerAngles = AimCircle.transform.localEulerAngles;
+        localEulerAngles.y = -MathUtils.AngleTo(Camera.WorldToScreenPoint(AimCircle.transform.position), Input.mousePosition);
+        AimCircle.transform.localEulerAngles = localEulerAngles;
+    }
+}
